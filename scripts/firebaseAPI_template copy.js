241cab629bef3b907e3520496c13c00c44f54f2b
Jason Chow <jchow149@my.bcit.ca>
2023-11-14 12:11:02 -0800

    Finish demo11

--- scripts/firebaseAPI_template copy.js
+++ scripts/firebaseAPI_template copy.js
@@ -1,18 +1,18 @@
-//----------------------------------------
-//  Your web app's Firebase configuration
-//----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+// //----------------------------------------
+// //  Your web app's Firebase configuration
+// //----------------------------------------
+// var firebaseConfig = {
+//     apiKey: ,
+//     authDomain: ,
+//     projectId: ,
+//     storageBucket: ,
+//     messagingSenderId: ,
+//     appId:
+// };
 
-//--------------------------------------------
-// initialize the Firebase app
-// initialize Firestore database if using it
-//--------------------------------------------
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// //--------------------------------------------
+// // initialize the Firebase app
+// // initialize Firestore database if using it
+// //--------------------------------------------
+// const app = firebase.initializeApp(firebaseConfig);
+// const db = firebase.firestore();
